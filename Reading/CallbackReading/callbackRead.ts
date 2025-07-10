8a2901100b5331c931cc5fb24e31b6883541fed2
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2025-07-11 00:58:19 +0530

    added callback read

---
+++ Reading/CallbackReading/callbackRead.ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+
+fs.open("./testing.txt", "r", (err, fd: number) => {
+  const buff = Buffer.alloc(24);
+  const size = fs.statSync("./testing.txt").size;
+  if (err) {
+    console.log(err);
+  }
+  console.log("Size of the file is : ", size);
+  fs.readFile(fd, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(data.toString("utf-8"));
+  });
+  fs.read(fd, buff, (err, bytesRead, buff) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(buff.toString("utf-8", 0, bytesRead));
+  });
+});
