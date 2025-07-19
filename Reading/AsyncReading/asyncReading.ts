9f172424a4530f838d8e2fd3749507ec99bd5bdc
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2025-07-19 15:53:09 +0530

    added some docs

--- Reading/AsyncReading/asyncReading.ts
+++ Reading/AsyncReading/asyncReading.ts
@@ -5,9 +5,16 @@ const fileHandler = await fs.open("./testing.txt" , "r")
 const buff = Buffer.alloc(24);
 
 //* Asynchronous execution of code.
+//^ Only the buffer's length bytes will be read + here we have to manually close the file.
 const chunkData = await fileHandler.read(buff);
-//* Automatically closes the file
+// await fileHandler.close();
+
+//* Automatically closes the file , and will read all the data buffer
 const wholeData = await fileHandler.readFile();
+
+await fileHandler.close()
+
+
 
 console.log(chunkData);
 console.log(wholeData);
--- Writing/CallbackWriting/write.ts
+++ Writing/CallbackWriting/write.ts
@@ -1,6 +1,8 @@
 import fs from "fs"
 import { Buffer } from "buffer"
 
+//* This write function takes the buffer of the data that we want to write , its different from the buffer that is used in read , it is for storing the data , but this buffer is what that is actually being written.
+
 fs.open("./writing.txt" , "w" , (err , fd) =>{
     const buff = Buffer.from("Hello my name is srajan");
     fs.write(fd , buff , 0 , (err , written , buff) => {
@@ -15,5 +17,5 @@ fs.open("./writing.txt" , "w" , (err , fd) =>{
             console.log("Some error occurred" , err);
         }
         console.log("File closed");
-    })
+    }) 
 })
